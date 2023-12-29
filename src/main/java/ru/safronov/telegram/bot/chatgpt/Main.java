2cec7d1848da7642206bf0a23cf28296269e38fe
Timofei Safronov <44961856+cliff-v@users.noreply.github.com>
2023-12-29 12:59:36 +0300

    Edit packaging

--- src/main/java/ru/safronov/telegram/bot/chatgpt/Main.java
+++ src/main/java/ru/safronov/telegram/bot/chatgpt/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         while(true) {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(5500);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
