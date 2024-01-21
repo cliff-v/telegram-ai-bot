929b1a05e406abf9ff58061cc8ff2ae40df4faf9
Timofey Safronov <44961856+VanClifff@users.noreply.github.com>
2024-01-22 01:47:12 +0300

    Add env props

--- src/main/java/ru/safronov/telegram/chatgptbot/ChatgptbotApplication.java
+++ src/main/java/ru/safronov/telegram/chatgptbot/ChatgptbotApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatgptbotApplication {
 	public static void main(String[] args) {
+		System.getenv().forEach((k, v) -> System.out.println(k + " :kotik: " + v));
 		SpringApplication.run(ChatgptbotApplication.class, args);
 	}
 }
