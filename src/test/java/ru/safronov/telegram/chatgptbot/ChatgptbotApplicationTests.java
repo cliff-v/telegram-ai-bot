5c22ecff084ec8d0910803a19429aacb4ee4c043
Timofey Safronov <44961856+VanClifff@users.noreply.github.com>
2024-01-21 23:48:43 +0300

    Add env props

--- src/test/java/ru/safronov/telegram/chatgptbot/ChatgptbotApplicationTests.java
+++ src/test/java/ru/safronov/telegram/chatgptbot/ChatgptbotApplicationTests.java
@@ -1,13 +1,18 @@
 package ru.safronov.telegram.chatgptbot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChatgptbotApplicationTests {
 
+	@Autowired
+	BotComponent botComponent;
+
 	@Test
 	void contextLoads() {
+		System.out.println(botComponent.getBotUsername());
 	}
 
 }
