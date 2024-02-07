e65f135153d3f62f3841cd975e3e37356eb8ce25
Timofey Safronov <44961856+VanClifff@users.noreply.github.com>
2024-02-07 04:34:40 +0300

    some test

--- src/main/java/ru/safronov/telegram/chatgptbot/BotComponent.java
+++ src/main/java/ru/safronov/telegram/chatgptbot/BotComponent.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
@@ -32,7 +35,11 @@ public class BotComponent extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getText());
+        User user = update.getMessage().getFrom();
+        String text = update.getMessage().getText();
+        System.out.println(user + " " + text);
+        sendText(user.getId(), "Привет, %s! Твой текст: %s".formatted(user.getFirstName(), text));
+        copyMessage(user.getId(), update.getMessage().getMessageId());
     }
 
     @Override
@@ -43,5 +50,30 @@ public class BotComponent extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return botToken;
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what)     //Message content
+                .build();
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+    public void copyMessage(Long who, Integer msgId){
+        CopyMessage cm = CopyMessage.builder()
+                .fromChatId(who.toString())  //We copy from the user
+                .chatId(who.toString())      //And send it back to him
+                .messageId(msgId)            //Specifying what message
+                .build();
+        try {
+            execute(cm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
